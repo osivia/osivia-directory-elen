999062db647bfec1bb625bad076564a1962c9f26
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2016-03-02 15:15:26 +0000

    Réorganisation de la menubar.

--- workspace/management/src/main/java/fr/toutatice/workspace/portail/gestionWorkspace/plugin/GestionWorkspacePlugin.java
+++ workspace/management/src/main/java/fr/toutatice/workspace/portail/gestionWorkspace/plugin/GestionWorkspacePlugin.java
@@ -14,9 +14,9 @@ import org.nuxeo.ecm.automation.client.model.Document;
 import org.osivia.portal.api.PortalException;
 import org.osivia.portal.api.context.PortalControllerContext;
 import org.osivia.portal.api.customization.CustomizationContext;
-import org.osivia.portal.api.internationalization.Bundle;
+import org.osivia.portal.api.locator.Locator;
+import org.osivia.portal.api.menubar.IMenubarService;
 import org.osivia.portal.api.menubar.MenubarDropdown;
-import org.osivia.portal.api.menubar.MenubarGroup;
 import org.osivia.portal.api.menubar.MenubarItem;
 import org.osivia.portal.core.cms.CMSException;
 import org.osivia.portal.core.cms.CMSExtendedDocumentInfos;
@@ -32,104 +32,117 @@ import fr.toutatice.portail.cms.nuxeo.api.domain.AbstractPluginPortlet;
 import fr.toutatice.portail.cms.nuxeo.api.domain.IMenubarModule;
 
 /**
- * @author loic
- *
+ * Gestion workspace plugin.
+ * 
+ * @author Loïc Billon
+ * @see AbstractPluginPortlet
+ * @see IMenubarModule
  */
-public class GestionWorkspacePlugin extends AbstractPluginPortlet implements IMenubarModule  {
+public class GestionWorkspacePlugin extends AbstractPluginPortlet implements IMenubarModule {
 
-
-
-    /** Logger. */
-    private static final Log LOGGER = LogFactory.getLog(GestionWorkspacePlugin.class);
-
+    /** Customizer name. */
     private static final String CUSTOMIZER_NAME = "gestionworkspace.plugin";
 
 
+    /** Log. */
+    private final Log log;
 
-	/* (non-Javadoc)
-	 * @see fr.toutatice.portail.cms.nuxeo.api.domain.AbstractPluginPortlet#customizeCMSProperties(java.lang.String, org.osivia.portal.api.customization.CustomizationContext)
-	 */
-	@Override
-	protected void customizeCMSProperties(String arg0, CustomizationContext ctx) {
+    /** Menubar service. */
+    private final IMenubarService menubarService;
 
-		List<IMenubarModule> menubars = this.getMenubars(ctx);
-
-		menubars.add(this);
-
-	}
-
-	/* (non-Javadoc)
-	 * @see fr.toutatice.portail.cms.nuxeo.api.domain.IMenubarModule#adaptContentMenuBar(org.osivia.portal.core.cms.CMSServiceCtx, java.util.List)
-	 */
-	@Override
-    public void adaptContentMenuBar(CMSServiceCtx ctx, List<MenubarItem> menuBar, CMSPublicationInfos pubInfos, CMSExtendedDocumentInfos extendedDocumentInfos) {
-
-		if(ctx.getDoc() != null) {
-
-	        // Current document
-	        Document document = (Document) ctx.getDoc();
-
-	        if(document.getType().equals("Workspace")) {
-
-
-	            try {
-			        if(!this.isUserWorkspace(ctx, document) && pubInfos.isManageableByUser() && ContextualizationHelper.isCurrentDocContextualized(ctx)) {
-
-		            	// --------- EDIT LDAP MEMBERS
-
-		        		Map<String, String> windowProperties = new HashMap<String, String>();
-		        		windowProperties.put("osivia.ajaxLink", "1");
-		        		windowProperties.put("theme.dyna.partial_refresh_enabled", "true");
-		        		windowProperties.put("action", "consulterRole");
-
-
-		        		PortalControllerContext pcc = new PortalControllerContext(this.getPortletContext(), ctx.getRequest(), ctx.getResponse());
-
-						windowProperties.put("osivia.title", this.getMessage(pcc ,"MANAGE_MEMBERS_ACTION"));
-		        		//windowProperties.put("osivia.hideTitle", "1");
-		        		windowProperties.put("workspacePath", document.getPath());
-
-		        		String urlEditMembers = this.getPortalUrlFactory().getStartPortletUrl(pcc, DirectoryPortlets.gestionWorkspaces.getInstanceName(), windowProperties, false);
-
-		        		MenubarDropdown parent = this.getOtherOptionsDropdown(pcc, this.getBundleFactory().getBundle(ctx.getRequest().getLocale()));
-
-		                MenubarItem manageMembersItem = new MenubarItem("MANAGE_MEMBERS", this.getMessage(pcc, "MANAGE_MEMBERS_ACTION"), "glyphicons glyphicons-group",
-		                        parent, 22, urlEditMembers, null, null, null);
-			            manageMembersItem.setAjaxDisabled(true);
-
-			            menuBar.add(manageMembersItem);
-
-				     }
-			    }
-		        catch(PortalException ex) {
-		        	LOGGER.warn(ex.getMessage());
-		        } catch (CMSException ex) {
-		        	LOGGER.warn(ex.getMessage());
-				}
-	        }
-
-		}
-
-
-	}
 
     /**
-	 * Is document a user workspace ?
-	 * @param document
-     * @param ctx
-     * @return true if is it
-     * @throws CMSException
-	 */
-	private boolean isUserWorkspace(CMSServiceCtx ctx, Document document) throws CMSException {
+     * Constructor.
+     */
+    public GestionWorkspacePlugin() {
+        super();
+        this.log = LogFactory.getLog(this.getClass());
 
+        // Menubar service
+        this.menubarService = Locator.findMBean(IMenubarService.class, IMenubarService.MBEAN_NAME);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void customizeCMSProperties(String id, CustomizationContext context) {
+        List<IMenubarModule> menubars = this.getMenubars(context);
+        menubars.add(this);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void adaptContentMenuBar(CMSServiceCtx cmsContext, List<MenubarItem> menubar, CMSPublicationInfos pubInfos,
+            CMSExtendedDocumentInfos extendedDocumentInfos) {
+        if (cmsContext.getDoc() != null) {
+            // Current document
+            Document document = (Document) cmsContext.getDoc();
+
+            if ("Workspace".equals(document.getType())) {
+                try {
+                    if (!this.isUserWorkspace(cmsContext, document) && pubInfos.isManageableByUser()
+                            && ContextualizationHelper.isCurrentDocContextualized(cmsContext)) {
+
+                        // --------- EDIT LDAP MEMBERS
+                        Map<String, String> windowProperties = new HashMap<String, String>();
+                        windowProperties.put("osivia.ajaxLink", "1");
+                        windowProperties.put("theme.dyna.partial_refresh_enabled", "true");
+                        windowProperties.put("action", "consulterRole");
+
+                        PortalControllerContext portalControllerContext = new PortalControllerContext(this.getPortletContext(), cmsContext.getRequest(),
+                                cmsContext.getResponse());
+
+                        windowProperties.put("osivia.title", this.getMessage(portalControllerContext, "MANAGE_MEMBERS_ACTION"));
+                        // windowProperties.put("osivia.hideTitle", "1");
+                        windowProperties.put("workspacePath", document.getPath());
+
+                        String urlEditMembers = this.getPortalUrlFactory().getStartPortletUrl(portalControllerContext,
+                                DirectoryPortlets.gestionWorkspaces.getInstanceName(),
+                                windowProperties, false);
+
+                        MenubarDropdown parent = this.menubarService.getDropdown(portalControllerContext, MenubarDropdown.CMS_EDITION_DROPDOWN_MENU_ID);
+
+                        MenubarItem manageMembersItem = new MenubarItem("MANAGE_MEMBERS", this.getMessage(portalControllerContext, "MANAGE_MEMBERS_ACTION"),
+                                "glyphicons glyphicons-group", parent, 100, urlEditMembers, null, null, null);
+                        manageMembersItem.setAjaxDisabled(true);
+                        manageMembersItem.setDivider(true);
+
+                        menubar.add(manageMembersItem);
+                    }
+                } catch (PortalException ex) {
+                    log.warn(ex.getMessage());
+                } catch (CMSException ex) {
+                    log.warn(ex.getMessage());
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Check if current document is a user workspace.
+     * 
+     * @param cmsContext CMS context
+     * @param document current document
+     * @return true if current document is a user workspace
+     * @throws CMSException
+     */
+    private boolean isUserWorkspace(CMSServiceCtx cmsContext, Document document) throws CMSException {
+        // CMS service
         ICMSService cmsService = NuxeoController.getCMSService();
 
+        // Path
         String path = document.getPath() + "/";
 
         // Check if current item is not in user workspaces
         boolean userWorkspace = false;
 
-		List<CMSItem> userWorkspaces = cmsService.getWorkspaces(ctx, true, false);
+        List<CMSItem> userWorkspaces = cmsService.getWorkspaces(cmsContext, true, false);
         for (CMSItem cmsItem : userWorkspaces) {
             if (StringUtils.startsWith(path, cmsItem.getPath() + "/")) {
                 userWorkspace = true;
@@ -138,33 +151,14 @@ public class GestionWorkspacePlugin extends AbstractPluginPortlet implements IMe
         }
 
         return userWorkspace;
-	}
-
-	/**
-     * Get menubar other options dropdown menu.
-     *
-     * @param portalControllerContext portal controller context
-     * @param bundle internationalization bundle
-     * @return menubar dropdown menu
-     */
-    private MenubarDropdown getOtherOptionsDropdown(PortalControllerContext portalControllerContext, Bundle bundle) {
-        MenubarDropdown dropdown = this.getMenubarService().getDropdown(portalControllerContext, MenubarDropdown.OTHER_OPTIONS_DROPDOWN_MENU_ID);
-
-        if (dropdown == null) {
-            dropdown = new MenubarDropdown(MenubarDropdown.OTHER_OPTIONS_DROPDOWN_MENU_ID, bundle.getString("OTHER_OPTIONS"), "glyphicons glyphicons-option-vertical",
-                    MenubarGroup.GENERIC, 40);
-            dropdown.setReducible(false);
-            this.getMenubarService().addDropdown(portalControllerContext, dropdown);
-        }
-
-        return dropdown;
     }
 
-	/* (non-Javadoc)
-	 * @see fr.toutatice.portail.cms.nuxeo.api.domain.AbstractPluginPortlet#getPluginName()
-	 */
-	@Override
-	protected String getPluginName() {
-		return CUSTOMIZER_NAME;
-	}
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected String getPluginName() {
+        return CUSTOMIZER_NAME;
+    }
 }
