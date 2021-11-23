5c5e6002ebf3d2aed11f05a14a4db95056d0f0d1
Jean-Sébastien <jssteux@osivia.com>
2021-11-23 09:50:08 +0100

    procedure #1

--- person/creation/src/main/java/org/osivia/services/person/creation/plugin/service/PersonCreationPluginServiceImpl.java
+++ person/creation/src/main/java/org/osivia/services/person/creation/plugin/service/PersonCreationPluginServiceImpl.java
@@ -1,11 +1,14 @@
 package org.osivia.services.person.creation.plugin.service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.commons.lang.StringUtils;
 import org.osivia.directory.v2.service.PersonUpdateService;
@@ -44,7 +47,19 @@ public class PersonCreationPluginServiceImpl implements PersonCreationPluginServ
     /** Mail pattern. */
     private final Pattern mailPattern;
 
+    
+    /** Person identifier length. */
+    int PERSON_UID_LENGTH = 10;
 
+    
+    /** Base 62 alphabet conversion table. */
+    private static final char[] TO_BASE_62 = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+            'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    /** Base 62. */
+    private static final int BASE_62 = 62;
+    
     /**
      * Constructor.
      */
@@ -85,7 +100,8 @@ public class PersonCreationPluginServiceImpl implements PersonCreationPluginServ
 
         if (person == null) {
             person = this.personService.getEmptyPerson();
-            person.setUid(mail);
+            String uid = generateUid();
+            person.setUid(uid);
             person.setCn(mail);
             person.setSn(mail);
             person.setMail(mail);
@@ -95,6 +111,7 @@ public class PersonCreationPluginServiceImpl implements PersonCreationPluginServ
             String password = RandomStringUtils.randomAlphanumeric(8);
             String passwordVariableName = context.getParamValue(executor, PASSWORD_FILTER_VARIABLE_NAME);
             variables.put(passwordVariableName, password);
+            variables.put("uid", uid);
             this.personService.updatePassword(person, password);
         } else {
             String message = bundle.getString("PERSON_CREATION_FORM_FILTER_MESSAGE_ERROR_ALREADY_EXISTS");
@@ -102,4 +119,39 @@ public class PersonCreationPluginServiceImpl implements PersonCreationPluginServ
         }
     }
 
+    
+    /**
+     * Generate person identifier.
+     *
+     * @return identifier
+     */
+    private String generateUid() {
+        // Random generator
+        Random random = new Random();
+
+        // Search criteria
+        Person criteria = this.personService.getEmptyPerson();
+        // Search results
+        List<Person> results = null;
+
+        // Person identifier
+        String uid = null;
+
+        while (StringUtils.isEmpty(uid) || CollectionUtils.isNotEmpty(results)) {
+            // Generate new identifier
+            Character[] array = new Character[PERSON_UID_LENGTH];
+            for (int i = 0; i < PERSON_UID_LENGTH; i++) {
+                array[i] = TO_BASE_62[random.nextInt(BASE_62)];
+            }
+            uid = StringUtils.join(array);
+
+            // Check identifier uniqueness
+            criteria.setUid(uid);
+            results = this.personService.findByCriteria(criteria);
+        }
+
+        return uid;
+    }
+    
+    
 }
